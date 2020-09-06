31212e183fa426672bb161e646bfe45bc3d3ff85
crudaz <romerocruz@gmail.com>
2020-09-06 01:55:06 -0500

    feat: routing pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./App.scss";
+import { Route, BrowserRouter } from "react-router-dom";
+
+import { pageTitle, navLinks } from "./config";
+
+// Components
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Voting App</header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header title={pageTitle} items={navLinks} />
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
