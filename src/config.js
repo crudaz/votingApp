d25edab11709b2d1a2194a5fdc052c57eea1bbfd
crudaz <romerocruz@gmail.com>
2020-09-06 01:54:34 -0500

    feat: config page json

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const pageTitle = "Rule of Thumb.";
+export const navLinks = [
+  {
+    name: "Past Trials",
+    link: "pastTrials",
+  },
+  {
+    name: "How It Works",
+    link: "howitworks",
+  },
+  {
+    name: "Log In/Sign Up",
+    link: "login",
+  },
+];
